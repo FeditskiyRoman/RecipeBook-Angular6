7470d275c664db6ac25640f3716749f55bb3bd46
Roman Feditskiy <feditskiy@gmail.com>
2018-11-21 16:03:10 +0200

    Added fetching data

--- src/app/header/header.components.ts
+++ src/app/header/header.components.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HearedComponent {
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        (response: Response) => console.log('Success!')
+      );
+  }
+
+  onFetchdata() {
+    this.dataStorageService.getRecipes();
+  }
 }
